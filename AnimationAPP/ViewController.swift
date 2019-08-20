30c0164d9525c4389e95c447b8c08b1a5b709464
John Heifetz <skidheif@gmail.com>
2019-08-20 17:03:32 +0300

    add some code

--- AnimationAPP/ViewController.swift
+++ AnimationAPP/ViewController.swift
@@ -6,15 +6,44 @@
 //  Copyright © 2019 heif_team. All rights reserved.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    @IBOutlet var springView: SpringView!
+    @IBOutlet var viewLabel: UILabel!
+    @IBOutlet var springButton: SpringButton!
+    
+    private var countOfTap = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        springView.animate()
+        
     }
 
-
+    @IBAction func springButtonTapped(_ sender: SpringButton) {
+        switch countOfTap {
+        case 0:
+            springView.animation = "wobble"
+            springView.curve = "easyOut"
+            springView.force = 5.0
+            springView.duration = 3.0
+            springView.rotate = 3.0
+            springView.animate()
+            viewLabel.text = "springView.animation = 'wobble'\n springView.curve = 'easyOut'\n springView.force = 5.0\n springView.duration = 3.0\n springView.rotate = 3.0"
+            springButton.setTitle("Wooble Animation", for: .normal)
+            countOfTap += 1
+        case 1:
+            springView.animation = "fadeIn"
+            springView.curve = "easyInQuad"
+            springView.force = 2.0
+            springView.duration = 3.5
+            springView.rotate = 4.0
+            springView.animate()
+            viewLabel.text = "springView.animation = 'fadeIn'\n springView.curve = 'easyInQuad'\n springView.force = 2.0\n springView.duration = 3.5\n springView.rotate = 4.0"
+            countOfTap -= 1
+        default:
+            print("ERROR")
+        }
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AnimationAPP' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Spring', :git => 'https://github.com/MengTo/Spring.git'
+
+end
